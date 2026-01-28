057e633dd0e02b000edbd56278d0d1b16538307f
Aradhya <soularadhya@gmail.com>
2026-01-28 21:33:07 +0530

    Update GSAP playground code

--- ultimate-gsap-starter/pages/02a_understanding-properties/script.js
+++ ultimate-gsap-starter/pages/02a_understanding-properties/script.js
@@ -3,7 +3,7 @@ import gsap from 'gsap';
 gsap.to('.box', {
   opacity: 1,
   rotation: 360,
-background: '#ff6f61',
+background: '#1b93c7ff',
 borderRadius:'50%',
 scale:1.25,
 duration:2,
---
+++ ultimate-gsap-starter/pages/04a_sine-easing/script.js
@@ -0,0 +1,26 @@
+import gsap from 'gsap';
+
+const button = document.querySelector('.scroll-to-top');
+
+window.addEventListener('scroll', function(){
+  if(this.window.scrollY>500){
+    button.classList.add('show');
+
+    if(!isFloating) {
+      gsap.to(button, {
+        y: -10,
+        duration: 1.5,
+        repeat: -1,
+        yoyo: true,
+        ease: "sine.Out"
+      } )
+
+      isFloating= true;
+    }else {
+      button.classList.remove('show')
+    }
+  }
+
+
+
+})
