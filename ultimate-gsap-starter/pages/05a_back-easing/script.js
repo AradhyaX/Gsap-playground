3357f1c097409febcda6ab5df728f881b0b32505
Aradhya <soularadhya@gmail.com>
2026-01-30 11:58:43 +0530

    Add GSAP animation with proper bounds calculation and back easing

--- ultimate-gsap-starter/pages/05a_back-easing/script.js
+++ ultimate-gsap-starter/pages/05a_back-easing/script.js
@@ -5,8 +5,18 @@ const indicator = document.querySelector('.indicator');
 const tabRow = document.querySelector('.tab-row');
 
 const updateIndicator = (target) => {
-  // Animation logic to be added
-  console.log("Updating indicator for:", target);
+  const tabBounds = target.getBoundingClientRect();
+  const rowBounds = tabRow.getBoundingClientRect();
+
+  const width = tabBounds.width;
+  const offset = tabBounds.left - rowBounds.left;
+
+  gsap.to(indicator, {
+    x: offset,
+    width: width,
+    duration: 0.4,
+    ease: 'back.out(1.7)',
+  });
 };
 
 tabs.forEach((tab) => {
