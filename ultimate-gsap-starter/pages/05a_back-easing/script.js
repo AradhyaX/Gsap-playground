21f0dedba6f76870aa7c887befb6f8556fffb095
Aradhya <soularadhya@gmail.com>
2026-01-30 11:57:59 +0530

    Initialize GSAP back-easing script with DOM selectors

---
+++ ultimate-gsap-starter/pages/05a_back-easing/script.js
@@ -0,0 +1,8 @@
+import gsap from "gsap";
+
+const tabs = document.querySelectorAll('.tab');
+const indicator = document.querySelector('.indicator');
+const tabRow = document.querySelector('.tab-row');
+
+// Initial setup ensuring elements are selected
+console.log("Tabs initialized");
